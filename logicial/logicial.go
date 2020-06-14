e4380736a22591ad4fe7b1e1310a1b6d7cff85eb
Evsio0n <1344729087@qq.com>
2020-06-14 18:11:54 +0800

    开始写入逻辑

---
+++ logicial/logicial.go
@@ -0,0 +1,65 @@
+package logicial
+
+func doLogin() {
+
+}
+
+///
+//
+///
+func doLoginWithToken() {
+
+}
+
+///
+//
+///
+func returnToken() {
+
+}
+
+///
+//
+///
+func getUnread() {
+
+}
+
+///
+//
+///
+func getUnreadList() {
+
+}
+
+///
+//
+///
+func getOnlineStatus() {
+
+}
+
+///
+//
+///
+func keepAlive() {
+
+}
+
+///
+//
+///
+func getUserList() {
+
+}
+
+///
+//
+///
+func messageWasRead() {
+
+}
+
+///
+//
+///
---
+++ logicial/typeo.go
@@ -0,0 +1,56 @@
+package logicial
+
+import (
+	"time"
+)
+
+type loginQueue struct {
+	uuid string
+	sid  string
+}
+
+type tokenReturned struct {
+	token string
+}
+
+type onlineStatus struct {
+	online bool
+	sid    int
+}
+type chanOnLineStatus chan<- onlineStatus
+
+type checkOnlineStatus struct {
+	online bool
+	sid    int
+	token  string
+}
+type chanCheckOnlineStatus chan<- checkOnlineStatus
+
+type getUnreadNumbers struct {
+	sid   int
+	token string
+}
+type chanGetUnreadNumbers chan<- getUnreadNumbers
+
+type unreadNumbers struct {
+	badge int
+}
+type chanUnreadNumbers chan<- unreadNumbers
+
+type unreadMessage struct {
+	messageContent string
+	messageGen     int
+	messageDate    time.Time
+	messageRead    bool
+}
+type checkReadMessage struct {
+	messageGen int
+	token      string
+}
+type messagePerPerson map[string]unreadMessage
+
+//["王册全",{"你好","33223344","Time",true}]
+
+type messageUnread map[string]messagePerPerson
+
+//["idUnread",["王册全",{"你好","33223344","Time"}]
